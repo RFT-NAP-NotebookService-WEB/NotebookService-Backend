2ced5570f41f25e63b79d842c653ff728e9266b5
Unknown <pap.kristof.199411@gmail.com>
2018-10-22 14:36:44 +0200

    Spring Application

---
+++ notebookservice-parent/notebookservice-backend/src/main/java/hu/unideb/inf/notebookservice/backend/config/Application.java
@@ -0,0 +1,24 @@
+package hu.unideb.inf.notebookservice.backend.config;
+
+import hu.unideb.inf.notebookservice.persistence.config.PersistenceConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(PersistenceConfiguration.class)
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(final String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+//    @Override
+//    protected final SpringApplicationBuilder configure(
+//            final SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
+}
---
+++ notebookservice-parent/notebookservice-backend/src/main/java/hu/unideb/inf/notebookservice/backend/config/WebConfiguration.java
@@ -0,0 +1,51 @@
+package hu.unideb.inf.notebookservice.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import hu.unideb.inf.notebookservice.service.config.ServiceConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@Configuration
+@ComponentScan("hu.unideb.inf.notebookservice.backend")
+@Import(ServiceConfiguration.class)
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    /**
+     * @param converters message converter.
+     */
+    @Override
+    public final void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
+    /**
+     * @return mapped object.
+     */
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    /**
+     * @return converted object mapper.
+     */
+    @Bean
+    public MappingJackson2HttpMessageConverter
+    mappingJackson2HttpMessageConverter() {
+
+        return new MappingJackson2HttpMessageConverter(objectMapper());
+    }
+}
