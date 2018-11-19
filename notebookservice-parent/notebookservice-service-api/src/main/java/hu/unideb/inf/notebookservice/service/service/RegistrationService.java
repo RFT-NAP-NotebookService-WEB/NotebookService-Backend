d1cd285cacee35e30c2e82c9136c5c0adc383eff
EvET <pap.kristof.199411@gmail.com>
2018-11-19 15:34:20 +0100

    Big Refactor

--- notebookservice-parent/notebookservice-service-api/src/main/java/hu/unideb/inf/notebookservice/service/service/RegistrationService.java
+++
@@ -1,8 +0,0 @@
-package hu.unideb.inf.notebookservice.service.service;
-
-import hu.unideb.inf.notebookservice.commons.pojo.request.RegistrationRequest;
-
-public interface RegistrationService {
-
-    void register(RegistrationRequest registrationRequest);
-}
