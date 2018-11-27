30dcdc4b0319848c2425c4419b4444bdb910cb4f
EvET <pap.kristof.199411@gmail.com>
2018-11-27 22:54:48 +0100

    Dockerfile

--- notebookservice-parent/notebookservice-web/src/main/java/hu/unideb/inf/notebookservice/web/config/Application.java
+++ notebookservice-parent/notebookservice-web/src/main/java/hu/unideb/inf/notebookservice/web/config/Application.java
@@ -5,10 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@Import({PersistenceConfiguration.class, SecurityConfiguration.class})
+@Import({PersistenceConfiguration.class, SecurityConfiguration.class, CustomCORSConfiguration.class})
 public class Application extends SpringBootServletInitializer {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
