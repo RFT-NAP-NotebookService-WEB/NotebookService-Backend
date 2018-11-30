4e06c6624eb390fafdaedd975705917ee84a5ce4
EvET <pap.kristof.199411@gmail.com>
2018-11-30 17:08:32 +0100

    Path refactor

---
+++ notebookservice-parent/notebookservice-commons/src/main/java/hu/unideb/inf/notebookservice/commons/path/UserPath.java
@@ -0,0 +1,10 @@
+package hu.unideb.inf.notebookservice.commons.path;
+
+public final class UserPath {
+
+    public static final String LOGIN_URL = "/login";
+
+    public static final String REGISTRATION_URL = "/register";
+
+    public static final String USERS_URL = "/users";
+}
