5f77a19c15ef33f99765ef92d8c9ecaaa6390321
EvET <pap.kristof.199411@gmail.com>
2018-11-30 19:25:28 +0100

    UserResponse...

---
+++ notebookservice-parent/notebookservice-commons/src/main/java/hu/unideb/inf/notebookservice/commons/response/UserResponse.java
@@ -0,0 +1,16 @@
+package hu.unideb.inf.notebookservice.commons.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String userRole;
+}
