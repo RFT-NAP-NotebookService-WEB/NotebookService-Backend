d110f5db9fad09bc758926ff721aa7225caf5538
EvET <pap.kristof.199411@gmail.com>
2018-12-10 20:35:07 +0100

    JWT extended time

--- notebookservice-parent/notebookservice-web/src/main/java/hu/unideb/inf/notebookservice/web/security/JWTConfiguration.java
+++ notebookservice-parent/notebookservice-web/src/main/java/hu/unideb/inf/notebookservice/web/security/JWTConfiguration.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class JWTConfiguration {
 
     public final static String SECRET ="superSecretPassword";
-    public final static Long EXPIRATION_TIME=600000L;
+    public final static Long EXPIRATION_TIME=3600000L;
     public final static String TOKEN_PREFIX="Bearer ";
     public final static String HEADER_STRING="Authorization";
 
